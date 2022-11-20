5d3cff294a06f9eae7624730e9cf255215b61248
Carlos Soto <csoto.a84@gmail.com>
2022-11-20 14:36:59 -0300

    actualizacion

--- Planta31.py
+++ Planta31.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+import os
+planta218 = pd.read_excel(r'C:\Users\carlo\Desktop\Desafio-Sunai\data_plantas_python_1_1.xlsx', parse_dates= ["fecha_im"] )
 
-planta218 = pd.read_excel(r'C:\Users\carlo\Desktop\desafio\data_plantas_python_1_1.xlsx', parse_dates= ["fecha_im"] )
-
-planta31 = pd.read_excel(r'C:\Users\carlo\Desktop\desafio\data_plantas_python_2.xlsx',parse_dates= ["fecha_im"] )
+planta31 = pd.read_excel(r'C:\Users\carlo\Desktop\Desafio-Sunai\data_plantas_python_2.xlsx',parse_dates= ["fecha_im"] )
 
 total = pd.concat([planta218, planta31], axis=0)
 
@@ -14,7 +14,7 @@ planta31.plot.line(x='fecha_im', y='active_power_im')
 plt.xlabel("Fecha")
 plt.ylabel("Poder Activo")
 plt.title("Datos Planta 31")
-plt.savefig("c:/Users/carlo/Desktop/desafio/Grafico planta 31.png")
+plt.savefig("Grafico planta 31.png")
 
 
 """Paso 2"""
@@ -31,14 +31,20 @@ mins= planta31 ['active_power_im'].min()
 
 
 """archivo txt"""
+absFilepath =os.path.abspath(__file__)
+path, filename =os.path.split(absFilepath)
 s = sums
 ma = maxs
 mi = mins
+graf = open (path+"\Grafico planta 31.png")
+    
 with open("Datos Planta 31.txt", "w") as text_file:
     text_file.write("La suma diaria de active power en esta planta es: %s\n" % s)
     text_file.write("El active power maximo en esta planta es: %s\n" % maxs)
     text_file.write("El active power minimo en esta planta es: %s\n" % mins)
-    
+    text_file.write("El grafico de esta planta se encuentra en: %s\n"  % graf)
+    text_file.close
+
 """paso 3"""
 sumtotal= total['active_power_im'].sum()
 print("La suma total de active power en ambas plantas es:",sumtotal)
