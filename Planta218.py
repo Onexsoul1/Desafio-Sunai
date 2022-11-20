e380f7f8ea2dff4540605b5a20a2c0a6df19798d
Carlos Soto <csoto.a84@gmail.com>
2022-11-20 14:35:44 -0300

    ajuste

--- Planta218.py
+++ Planta218.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+import os
 
 planta218 = pd.read_excel(r'C:\Users\carlo\Desktop\desafio\data_plantas_python_1_1.xlsx', parse_dates= ["fecha_im"] )
 
@@ -30,16 +31,20 @@ mins= planta218 ['active_power_im'].min()
 
 
 """archivo txt"""
+absFilepath =os.path.abspath(__file__)
+path, filename =os.path.split(absFilepath)
+grafico = open (path+"\Grafico planta 218.png")
 s = sums
 ma = maxs
 mi = mins
-graf =("Grafico planta 218.png")
+
+graf=grafico
 with open("Datos Planta 218.txt", "w") as text_file:
     text_file.write("La suma diaria de active power en esta planta es: %s\n" % s)
     text_file.write("El active power maximo en esta planta es: %s\n" % maxs)
     text_file.write("El active power minimo en esta planta es: %s\n" % mins)
     text_file.write("El grafico de esta planta se encuentra en: %s\n" % graf)
-
+    text_file.close
 
 """paso 3"""
 sumtotal= total['active_power_im'].sum()
